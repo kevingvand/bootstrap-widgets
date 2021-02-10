4015e2fc800ce412ef918f2596d4d11d439558f3
Van Dommele Kevin Gillian (IFAT DCV DES CDF AMS VAL) <KevinGillian.VanDommele@infineon.com>
2021-02-11 00:52:02 +0100

    Added possibility to select items in treeview

--- TreeView/TreeView.js
+++ TreeView/TreeView.js
@@ -1,15 +1,23 @@
 $(function () {
 	$.widget("custom.treeView", {
-		options: {},
+		options: {
+			allowSelect: true,
+			leafSelectOnly: false,
+		},
 
 		_create: function () {
 			this.root = this.element;
+			this.selectedItem = null;
 
 			this._build();
 		},
 
 		_build: function () {
 			this.root.addClass("tree-view-list");
+
+			if (this.options.allowSelect)
+				this.root.addClass("tree-view-list-selectable");
+
 			this.root.find("ul").addClass("tree-view-sub-list");
 			this._processItems(this.root);
 		},
@@ -24,12 +32,31 @@ $(function () {
 				var itemText = $listItem.contents().get(0).nodeValue.trim();
 				var $subList = $listItem.children("ul");
 
-        var $listItemStatusIcon = $("<span>");
+				var $listItemStatusIcon = $("<span>");
 
 				var $listItemWrapper = $("<div>")
 					.css("padding-left", 10 + 20 * level + "px")
-          .append($listItemStatusIcon)
+					.append($listItemStatusIcon)
 					.append($("<span>").text(itemText));
+
+				if (
+					self.options.allowSelect &&
+					($subList.length === 0 || !self.options.leafSelectOnly)
+				) {
+					$listItemWrapper.click(function () {
+						var isSelected = $listItem.hasClass("tree-view-list-item-selected");
+						self.root
+							.find(".tree-view-list-item")
+							.removeClass("tree-view-list-item-selected");
+
+						self.selectedItem = null;
+
+						if (!isSelected) {
+							self.selectedItem = $listItem;
+							$listItem.addClass("tree-view-list-item-selected");
+						}
+					});
+				}
 
 				$listItem.empty().append($listItemWrapper);
 
@@ -38,23 +65,41 @@ $(function () {
 
 					self._processItems($subList, level + 1);
 
-          $listItemStatusIcon.empty().append($("<i>").addClass("fas fa-minus mr-2"));
+					$listItemStatusIcon
+						.empty()
+						.append($("<i>").addClass("fas fa-minus mr-2"));
 
 					$listItemWrapper
 						.addClass("tree-view-list-item-parent")
-
 						.click(function () {
-              var collapsed = $subList.is(":hidden");
-
-              $listItemStatusIcon.empty().append($("<i>").addClass("fas mr-2").addClass(collapsed ? "fa-minus" : "fa-plus"));
-
-              $subList.toggle();
+							self._toggleSubList($subList, $listItemStatusIcon);
 						});
+
+					if ($listItem.data("collapsed"))
+						self._toggleSubList($subList, $listItemStatusIcon);
 				} else {
-          $listItemStatusIcon.empty().append($("<i>").addClass("far fa-xs fa-angle-right mr-2"));
+					$listItemStatusIcon
+						.empty()
+						.append($("<i>").addClass("far fa-xs fa-angle-right mr-2"));
 				}
 			});
 		},
+
+		_toggleSubList($subList, $listItemStatusIcon) {
+			var collapsed = $subList.is(":hidden");
+
+			$listItemStatusIcon.empty().append(
+				$("<i>")
+					.addClass("fas mr-2")
+					.addClass(collapsed ? "fa-minus" : "fa-plus")
+			);
+
+			$subList.toggle();
+		},
+
+    selected: function() {
+      return this.selectedItem;
+    },
 
 		_destroy: function () {},
 	});
